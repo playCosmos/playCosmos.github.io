6aad88ee22d8183ba87141008ebf933b26b315fc
playCosmos <33890422+playCosmos@users.noreply.github.com>
2023-06-30 13:09:52 +0900

    Update Action.js

--- Air/Action.js
+++ Air/Action.js
@@ -1,3 +1,3 @@
 const el = document.querySelector("#article-info")
-const observer = new IntersectionObserver( ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),  { threshold: [1] });
+const observer = new IntersectionObserver( ([e]) => e.target.classList.toggle("isSticked", e.intersectionRatio < 1),  { threshold: [1] });
 observer.observe(el)
