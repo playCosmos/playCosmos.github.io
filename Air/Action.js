2d30d2e9bcc91763f7810f3c9776797ba9beda01
playCosmos <33890422+playCosmos@users.noreply.github.com>
2023-07-10 14:11:53 +0900

    Update Action.js

--- Air/Action.js
+++ Air/Action.js
@@ -4,4 +4,19 @@ function sel(selector) {
 };
 
 const observer = new IntersectionObserver(([e]) => e.target.classList.toggle("isSticked", e.intersectionRatio < 1), { threshold: [1] });
-document.addEventListener("DOMContentLoaded", function() { observer.observe(sel("div#article-info")); });
+
+const a1 = new document.createElement('div');
+const a2 = new document.createElement('div');
+
+document.addEventListener("DOMContentLoaded", function() {
+    var article_info = sel("div#article-info");
+    observer.observe(article_info);
+    article_info.before(a1);
+    a1.setAttribute("id", "stick1");
+    a1.prepend(article_info);
+    
+    var article_list = sel("#article-list");
+    article_list.before(a2);
+    a2.setAttribute("id", "stick2");
+    a2.prepend(article_list);
+});
