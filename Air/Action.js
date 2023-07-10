4b1f85ce531dd4a7f839b84d4e5405438cfee4dd
playCosmos <33890422+playCosmos@users.noreply.github.com>
2023-07-10 17:27:32 +0900

    Create Action.js

---
+++ Air/Action.js
@@ -0,0 +1,31 @@
+function sel(selector) {
+    let target = document.querySelector(selector);
+    return target;
+};
+
+const observer = new IntersectionObserver(([e]) => e.target.classList.toggle("isSticked", e.intersectionRatio < 1), { threshold: [1] });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var aDiscord = document.createElement('a');
+    aDiscord.setAttribute('href', "https://discord.gg/CE6bWsQ7QH");
+    aDiscord.innerText = " 디스코드";
+    
+    var iDiscord = document.createElement('i');
+    iDiscord.setAttribute('class', "fab fa-discord");
+    aDiscord.prepend(iDiscord);
+    
+    var aInstagram = document.createElement('a');
+    aInstagram.setAttribute('href', "https://www.instagram.com/million_air_");
+    aInstagram.innerText = " 인스타그램";
+    
+    var iInstagram = document.createElement('i');
+    iInstagram.setAttribute('class', "fab fa-instagram");
+    aInstagram.prepend(iInstagram);
+    
+    var fav = sel("a#addFavoriteBtn");
+    fav.after(aInstagram);
+    fav.after(aDiscord);    
+
+    var article_info = sel("div#article-info");
+    observer.observe(article_info);
+});
