25dfa88ec39a00f9ecbd9ece044bfedafd5bbb47
playCosmos <33890422+playCosmos@users.noreply.github.com>
2023-07-10 16:41:59 +0900

    Update Action.js

--- Air/Action.js
+++ Air/Action.js
@@ -6,9 +6,6 @@ function sel(selector) {
 const observer = new IntersectionObserver(([e]) => e.target.classList.toggle("isSticked", e.intersectionRatio < 1), { threshold: [1] });
 
 document.addEventListener("DOMContentLoaded", function() {
-    // var a1 = document.createElement('div');
-    // var a2 = document.createElement('div');
-    
     var aDiscord = document.createElement('a');
     aDiscord.setAttribute('href', "https://discord.gg/CE6bWsQ7QH");
     aDiscord.innerText = " 디스코드";
@@ -31,12 +28,4 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var article_info = sel("div#article-info");
     observer.observe(article_info);
-    // article_info.before(a1);
-    // a1.setAttribute("id", "stick1");
-    // a1.prepend(article_info);
-    
-    // var article_list = sel("#article-list");
-    // article_list.before(a2);
-    // a2.setAttribute("id", "stick2");
-    // a2.prepend(article_list);
 });
