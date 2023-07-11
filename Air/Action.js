dfb0f0dc93094aa8753297263ca3315218b1ba57
playCosmos <33890422+playCosmos@users.noreply.github.com>
2023-07-11 13:07:24 +0900

    Update Action.js

--- Air/Action.js
+++ Air/Action.js
@@ -6,32 +6,35 @@ function sel(selector) {
 const observer = new IntersectionObserver(([e]) => e.target.classList.toggle("isSticked", e.intersectionRatio < 1), { threshold: [1] });
 
 document.addEventListener("DOMContentLoaded", function() {
-    var aDiscord = document.createElement('a');
-    aDiscord.setAttribute('href', "https://discord.gg/CE6bWsQ7QH");
-    aDiscord.setAttribute('target', "_blank");
-    aDiscord.innerText = " 디스코드";
-    
-    var iDiscord = document.createElement('i');
-    iDiscord.setAttribute('class', "fab fa-discord");
-    aDiscord.prepend(iDiscord);
-    
-    var aInstagram = document.createElement('a');
-    aInstagram.setAttribute('href', "https://www.instagram.com/million_air_");
-    aInstagram.setAttribute('target', "_blank");
-    aInstagram.innerText = " 인스타그램";
-    
-    var iInstagram = document.createElement('i');
-    iInstagram.setAttribute('class', "fab fa-instagram");
-    aInstagram.prepend(iInstagram);
-    
     var info = sel("#board-info-bottom");
-    info.append(aDiscord);
-    info.append(aInstagram);
+    if (info != null)
+    {
+        var aDiscord = document.createElement('a');
+        aDiscord.setAttribute('href', "https://discord.gg/CE6bWsQ7QH");
+        aDiscord.setAttribute('target', "_blank");
+        aDiscord.innerText = " 디스코드";
+        
+        var iDiscord = document.createElement('i');
+        iDiscord.setAttribute('class', "fab fa-discord");
+        aDiscord.prepend(iDiscord);
+        
+        var aInstagram = document.createElement('a');
+        aInstagram.setAttribute('href', "https://www.instagram.com/million_air_");
+        aInstagram.setAttribute('target', "_blank");
+        aInstagram.innerText = " 인스타그램";
+        
+        var iInstagram = document.createElement('i');
+        iInstagram.setAttribute('class', "fab fa-instagram");
+        aInstagram.prepend(iInstagram);
 
-    var article_option_area = sel("#article-option-area");
-    article_option_area.setAttribute('title', "공지 숨김/보기");
-    info.append(article_option_area);
-
+        info.append(aDiscord);
+        info.append(aInstagram);
+        
+        var article_option_area = sel("#article-option-area");    
+        article_option_area.setAttribute('title', "공지 숨김/보기");
+        info.append(article_option_area);
+    }
+    
     var article_info = sel("div#article-info");
     if (article_info != null)
         observer.observe(article_info);
