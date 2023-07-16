511ade5fcc73406daa85db0d7c17fdab9e81de6f
playCosmos <33890422+playCosmos@users.noreply.github.com>
2023-07-16 14:58:34 +0900

    Update mAction.js

--- Air/mAction.js
+++ Air/mAction.js
@@ -1,34 +1,33 @@
 function sel(selector) {
-    let target = document.querySelector(selector);
-    return target;
+	let target = document.querySelector(selector);
+	return target;
 };
 
+function CreateSocialLink(iconClass, url, innerText = "") {
+	let ret = document.createElement('a');
+	ret.setAttribute('href', url);
+	ret.setAttribute('target', "_blank");
+	if ( innerText != "")
+		ret.innerText = " " + innerText;
+	
+	var icon = document.createElement('i');
+	icon.setAttribute('class', iconClass);
+	ret.prepend(icon);
+
+	return ret;
+};
+
+const 
+
 document.addEventListener("DOMContentLoaded", function() {
-	var aDiscord = document.createElement('a');
-	aDiscord.setAttribute('href', "https://discord.gg/CE6bWsQ7QH");
-	aDiscord.setAttribute('target', "_blank");
-	aDiscord.innerText = " 디스코드";
-	
-	var iDiscord = document.createElement('i');
-	iDiscord.setAttribute('class', "fab fa-discord");
-	aDiscord.prepend(iDiscord);
-	
-	var lDiscord = document.createElement('li');
-	lDiscord.prepend(aDiscord);
-	
-	var aInstagram = document.createElement('a');
-	aInstagram.setAttribute('href', "https://www.instagram.com/million_air_");
-	aInstagram.setAttribute('target', "_blank");
-	aInstagram.innerText = " 인스타그램";
-	
-	var iInstagram = document.createElement('i');
-	iInstagram.setAttribute('class', "fab fa-instagram");
-	aInstagram.prepend(iInstagram);
-    
-	var lInstagram = document.createElement('li');
-	lInstagram.prepend(aInstagram);
-	
 	var ul = sel("#board-info ul.dropdown-menu.dropdown-menu-right");
-	ul.prepend(lInstagram);
-	ul.prepend(lDiscord);
+	if (ul != null)
+	{
+		ul.prepend(CreateSocialLink("fab fa-discord", "https://discord.com/channels/@me/472262435261251594", "디스코드"));
+		ul.prepend(CreateSocialLink("fab fa-instagram", "https://www.instagram.com/million_air_", "인스타그램"));
+	}
+	
+	var theme_color = document.head.querySelector("[name~=theme-color]");
+	theme_color.setAttribute('content', "var(--maincolor)");
+	sel("head").append(theme_color);
 });
