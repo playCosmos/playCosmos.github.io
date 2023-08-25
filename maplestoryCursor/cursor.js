fed9fb5d071806de458d2c04c60db3cdde948d08
playCosmos <33890422+playCosmos@users.noreply.github.com>
2023-08-25 15:44:39 +0900

    Update cursor.js

--- maplestoryCursor/cursor.js
+++ maplestoryCursor/cursor.js
@@ -1,1 +1,9 @@
+function sel(selector) {
+    let ret = document.querySelector(selector);
+    return ret;
+};
 
+document.addEventListener("DOMContentLoaded", function() {
+  var body = sel("body");
+  body.addEventListener('mousedown', ()=> { text.innerHTML = `마우스 누르는중 `; });
+});
