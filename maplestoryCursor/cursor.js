03b15c7f19b19384936c0563d2d11c818e34045d
playCosmos <33890422+playCosmos@users.noreply.github.com>
2023-09-10 20:45:33 +0900

    Delete maplestoryCursor directory

--- maplestoryCursor/cursor.js
+++
@@ -1,9 +0,0 @@
-function sel(selector) {
-    let ret = document.querySelector(selector);
-    return ret;
-};
-
-document.addEventListener("DOMContentLoaded", function() {
-  var body = sel("body");
-  body.addEventListener('mousedown', ()=> { text.innerHTML = `마우스 누르는중 `; });
-});
